985284a846213b50db6df62150fc8e69d7b96d08
14sourabh7 <sourabhsingh@cedcoss.com>
2022-03-31 12:19:13 +0530

    jwt done

--- src/app/controllers/UserController.php
+++ src/app/controllers/UserController.php
@@ -61,21 +61,54 @@ class UserController extends Controller
     {
         $roles = Roles::find();
         $this->view->roles = $roles;
-
+        $this->view->tokenCheck = 0;
+        $this->view->msg = "";
         $check = $this->request->isPost();
         if ($check) {
             $inputs = $this->request->getPost();
 
-
-
             $user = new Users();
-            $user->name = $inputs['name'];
-            $user->email = $inputs['email'];
-            $user->password = $inputs['password'];
-            $user->role = $inputs['roles'];
-            $result = $user->save();
-            if ($result) {
-                $this->response->redirect('/product');
+            $user->checkUser($inputs['email'], $inputs['password']);
+            if ($checkUser) {
+                $this->view->msg = 'user exists you can login now or can use the token send to you to access';
+            } else {
+                $user->name = $inputs['name'];
+                $user->email = $inputs['email'];
+                $user->password = $inputs['password'];
+                $user->role = $inputs['roles'];
+                $result = $user->save();
+
+                if ($result) {
+                    $signer  = new Hmac();
+                    $builder = new Builder($signer);
+
+                    $now        = new DateTimeImmutable();
+                    $issued     = $now->getTimestamp();
+                    $notBefore  = $now->modify('-1 minute')->getTimestamp();
+                    $expires    = $now->modify('+1 day')->getTimestamp();
+                    $passphrase = 'QcMpZ&b&mo3TPsPk668J6QH8JA$&U&m2';
+
+
+                    // Setup
+                    $builder
+                        ->setAudience('localhost:8080')  // aud
+                        ->setContentType('application/json')        // cty - header
+                        ->setExpirationTime($expires)               // exp 
+                        ->setId('abcd123456789')                    // JTI id 
+                        ->setIssuedAt($issued)                      // iat 
+                        ->setIssuer('https://phalcon.io')           // iss 
+                        ->setNotBefore($notBefore)                  // nbf
+                        ->setSubject($inputs['roles'])   // sub
+                        ->setPassphrase($passphrase)                // password 
+                    ;
+
+                    // Phalcon\Security\JWT\Token\Token object
+                    $tokenObject = $builder->getToken();
+                    // The token
+                    $userToken =  $tokenObject->getToken();
+                    $this->view->tokenCheck = 1;
+                    $this->view->token = $userToken;
+                }
             }
         }
     }
