3937a653acae3734e15f5db5f0f01c9c9580c062
14sourabh7 <sourabhsingh@cedcoss.com>
2022-04-01 09:26:51 +0530

    jwt

--- src/app/controllers/UserController.php
+++ src/app/controllers/UserController.php
@@ -4,7 +4,8 @@ use Phalcon\Mvc\Controller;
 
 use Phalcon\Security\JWT\Builder;
 use Phalcon\Security\JWT\Signer\Hmac;
-
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 class UserController extends Controller
 {
@@ -72,50 +73,46 @@ class UserController extends Controller
             $inputs = $this->request->getPost();
 
             $user = new Users();
-            $user->checkUser($inputs['email'], $inputs['password']);
-            if ($checkUser) {
-                $this->view->msg = 'user exists you can login now or can use the token send to you to access';
-            } else {
-                $user->name = $escaper->sanitize($inputs['name']);
-                $user->email
-                    = $escaper->sanitize($inputs['email']);
-                $user->password =
-                    $escaper->sanitize($inputs['password']);
-                $user->role =
-                    $escaper->sanitize($inputs['roles']);
-                $result = $user->save();
 
-                if ($result) {
-                    $signer  = new Hmac();
-                    $builder = new Builder($signer);
+            $user->name = $escaper->sanitize($inputs['name']);
+            $user->email
+                = $escaper->sanitize($inputs['email']);
+            $user->password =
+                $escaper->sanitize($inputs['password']);
+            $user->role =
+                $escaper->sanitize($inputs['roles']);
+            $result = $user->save();
 
-                    $now        = new DateTimeImmutable();
-                    $issued     = $now->getTimestamp();
-                    $notBefore  = $now->modify('-1 minute')->getTimestamp();
-                    $expires    = $now->modify('+1 day')->getTimestamp();
-                    $passphrase = 'QcMpZ&b&mo3TPsPk668J6QH8JA$&U&m2';
+            if ($result) {
+                $signer  = new Hmac();
+                $builder = new Builder($signer);
+
+                $now        = new DateTimeImmutable();
+                $issued     = $now->getTimestamp();
+                $notBefore  = $now->modify('-1 minute')->getTimestamp();
+                $expires    = $now->modify('+1 day')->getTimestamp();
+                $passphrase = 'QcMpZ&b&mo3TPsPk668J6QH8JA$&U&m2';
 
 
-                    // Setup
-                    $builder
-                        ->setAudience('localhost:8080')  // aud
-                        ->setContentType('application/json')        // cty - header
-                        ->setExpirationTime($expires)               // exp 
-                        ->setId('abcd123456789')                    // JTI id 
-                        ->setIssuedAt($issued)                      // iat 
-                        ->setIssuer('https://phalcon.io')           // iss 
-                        ->setNotBefore($notBefore)                  // nbf
-                        ->setSubject($inputs['roles'])   // sub
-                        ->setPassphrase($passphrase)                // password 
-                    ;
+                // Setup
+                $builder
+                    ->setAudience('localhost:8080')  // aud
+                    ->setContentType('application/json')        // cty - header
+                    ->setExpirationTime($expires)               // exp 
+                    ->setId('abcd123456789')                    // JTI id 
+                    ->setIssuedAt($issued)                      // iat 
+                    ->setIssuer('https://phalcon.io')           // iss 
+                    ->setNotBefore($notBefore)                  // nbf
+                    ->setSubject($inputs['roles'])   // sub
+                    ->setPassphrase($passphrase)                // password 
+                ;
 
-                    // Phalcon\Security\JWT\Token\Token object
-                    $tokenObject = $builder->getToken();
-                    // The token
-                    $userToken =  $tokenObject->getToken();
-                    $this->view->tokenCheck = 1;
-                    $this->view->token = $userToken;
-                }
+                // Phalcon\Security\JWT\Token\Token object
+                $tokenObject = $builder->getToken();
+                // The token
+                $userToken =  $tokenObject->getToken();
+                $this->view->tokenCheck = 1;
+                $this->view->token = $userToken;
             }
         }
     }
